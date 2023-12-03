81b957b3008876bcaed89071838dcee5d443160c
HansAguilar <aguilarhans100701@gmail.com>
2023-12-04 00:11:29 +0800

    chat UI fixed

--- components/CreateMessageModal.jsx
+++ components/CreateMessageModal.jsx
@@ -20,7 +20,7 @@ function CreateMessageModal({ modal, setModal }) {
         messageContent: "",
         type: "CLIENT"
     });
-    
+
     const [suggestion, setSuggestion] = useState([]);
 
     const handleChange = (name, text) => {
@@ -56,71 +56,71 @@ function CreateMessageModal({ modal, setModal }) {
         setModal(false);
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(fetchAdmin());
-    },[])
+    }, [])
 
     return (
-       <>
-        {admin?.loading && <Loader loading={admin?.loading} />}
-        {
-            !admin?.loading && (
-                <View style={{ height: "100%", width: "100%", backgroundColor: "rgba(0, 0, 0, 0.5)", position: 'absolute', zIndex: 10, padding: 20, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <View style={{ backgroundColor: "white", padding: 10, height: "auto", width: "100%" }}>
-    
-                    <View>
-                        <Text style={{ fontSize: 14, color: "#595959" }}>Search Admin</Text>
-                        <TextInput
-                            value={messageData.adminName}
-                            style={{ width: "100%", borderWidth: 1, paddingVertical: 5, paddingHorizontal: 10, borderColor: "#ccc", borderRadius: 6 }}
-                            onChangeText={(text) => handleChange("adminName", text)}
-                            placeholder="Search admin..." />
-                        {
-                            suggestion?.length > 0 && (
-                                <View style={{ marginTop: 10 }}>
-                                    {
-                                        suggestion.map((val, idx) => (
-                                            <Text
-                                                key={idx}
-                                                style={{ padding: 10, width: "100%", borderBottomWidth: 1, borderBottomColor: "#ccc" }}
-                                                onPress={() => {
-                                                    setMessageData({ ...messageData, adminId: val.adminId, adminName: `Admin ${val.firstname}` });
-                                                    setSuggestion([]);
-                                                }}
-                                            >Admin {val.firstname}</Text>
-                                        ))
-                                    }
-                                </View>
-                            )
-                        }
-                    </View>
-    
-                    {
-                        messageData.adminId && messageData.adminName && (
-                            <View style={{ marginTop: 10 }}>
-                                <Text style={{ fontSize: 14, color: "#595959" }}>Message Content</Text>
+        <>
+            {admin?.loading && <Loader loading={admin?.loading} />}
+            {
+                !admin?.loading && (
+                    <View style={{ height: "100%", width: "100%", backgroundColor: "rgba(0, 0, 0, 0.5)", position: 'absolute', zIndex: 10, padding: 20, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <View style={{ backgroundColor: "white", padding: 10, height: "auto", width: "100%" }}>
+
+                            <View style={{gap: 6}}>
+                                <Text style={{ fontSize: 14, color: "#595959" }}>Search Admin</Text>
                                 <TextInput
-                                    style={{ height: 100, borderColor: 'gray', borderWidth: 1, borderColor: "#ccc", padding: 8, borderRadius: 6 }}
-                                    multiline
-                                    numberOfLines={4}
-                                    placeholder="Type your message here..."
-                                    value={messageData.messageContent}
-                                    onChangeText={(value) => handleChange("messageContent", value)}
-                                />
+                                    value={messageData.adminName}
+                                    style={{ width: "100%", borderWidth: 1, padding: 6, borderColor: "#ccc", borderRadius: 6 }}
+                                    onChangeText={(text) => handleChange("adminName", text)}
+                                    placeholder="Search admin..." />
+                                {
+                                    suggestion?.length > 0 && (
+                                        <View style={{ marginTop: 10 }}>
+                                            {
+                                                suggestion.map((val, idx) => (
+                                                    <Text
+                                                        key={idx}
+                                                        style={{ padding: 10, width: "100%", borderBottomWidth: 1, borderBottomColor: "#ccc" }}
+                                                        onPress={() => {
+                                                            setMessageData({ ...messageData, adminId: val.adminId, adminName: `Admin ${val.firstname}` });
+                                                            setSuggestion([]);
+                                                        }}
+                                                    >Admin {val.firstname}</Text>
+                                                ))
+                                            }
+                                        </View>
+                                    )
+                                }
                             </View>
-                        )
-                    }
-    
-                    <View style={{ width: "100%", display: 'flex', flexDirection: 'row', gap: 10, paddingTop: 10 }}>
-                        <Text style={{ flex: 1, textAlign: 'center', paddingVertical: 10, backgroundColor: "#ef4444", color: "#fff", borderRadius: 6 }} onPress={clearData}>Cancel</Text>
-                        <Text style={{ flex: 1, textAlign: 'center', paddingVertical: 10, backgroundColor: "#06b6d4", color: "#fff", borderRadius: 6 }} onPress={submitButton}>Save Changes</Text>
+
+                            {
+                                messageData.adminId && messageData.adminName && (
+                                    <View style={{ marginTop: 10 }}>
+                                        <Text style={{ fontSize: 14, color: "#595959" }}>Message Content</Text>
+                                        <TextInput
+                                            style={{ height: 100, borderColor: 'gray', borderWidth: 1, borderColor: "#ccc", padding: 8, borderRadius: 6 }}
+                                            multiline
+                                            numberOfLines={4}
+                                            placeholder="Type your message here..."
+                                            value={messageData.messageContent}
+                                            onChangeText={(value) => handleChange("messageContent", value)}
+                                        />
+                                    </View>
+                                )
+                            }
+
+                            <View style={{ width: "100%", display: 'flex', flexDirection: 'row', gap: 10, paddingTop: 10 }}>
+                                <Text style={{ flex: 1, textAlign: 'center', paddingVertical: 10, backgroundColor: "#ef4444", color: "#fff", borderRadius: 6 }} onPress={clearData}>Close</Text>
+                                <Text style={{ flex: 1, textAlign: 'center', paddingVertical: 10, backgroundColor: "#06b6d4", color: "#fff", borderRadius: 6 }} onPress={submitButton}>Search</Text>
+                            </View>
+                        </View>
+                        <Toast />
                     </View>
-                </View>
-                <Toast />
-            </View>
-            )
-        }
-       </>
+                )
+            }
+        </>
     );
 }
 
